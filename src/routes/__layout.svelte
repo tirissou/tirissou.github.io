bae05f6577dc377b62dababc5318a10d4c56b9af
Thibault Irissou <irissou.thibault@gmail.com>
2022-02-28 14:55:35 -0800

    clean homepage

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,17 +2,6 @@
     import "../app.css";
 </script>
 
-<script lang="ts">
-    import { prefetch } from "$app/navigation";
-</script>
-
-<nav class="p-6 bg-neutral-500 justify-between flex-wrap items-center m-4 rounded-md">
-    <div on:mouseover={() => prefetch("/blog")} class="flex mt-4 justify-end">
-	<a sveltekit:prefetch class="text-lg mr-4 text-neutral-200 hover:text-white" href="/">Home</a>
-	<a sveltekit:prefetch class="text-lg mr-4 text-neutral-200 hover:text-white" href="/blog">Blog</a>
-    </div>
-</nav>
-
 <main class="p-4">
     <slot />
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
     theme: {
 	extend: {
 	    colors: {
+		'lightshade':  '#F2F3F1',
+		'lightaccent': '#E98B75',
+		'main': '#E04629',
+		'darkaccent': '#999BBA',
+		'darkshade': '#151C2B'
+	    },
+	    fontFamily: {
+		'catchy': ['RobotoSerif', 'serif']
 	    }
 	},
     },
